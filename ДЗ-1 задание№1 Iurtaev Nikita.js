4f682e5b2adc16187944e7cbafe3c0af5b6c9995
Nik <133938672+NIK20-58@users.noreply.github.com>
2024-01-01 20:47:57 +0400

    Update ДЗ-1 задание№1 Iurtaev Nikita.js

--- ДЗ-1 задание№1 Iurtaev Nikita.js
+++ ДЗ-1 задание№1 Iurtaev Nikita.js
@@ -1,33 +1,11 @@
 let firstNum = prompt();
 let secondNum = prompt();
 
-
-
-if (firstNum.trim()!=='' && secondNum.trim()!=='' && !isNaN(firstNum) && !isNaN(secondNum)) {
+if (firstNum.trim() !== '' && secondNum.trim() !== '' && !isNaN(firstNum) && !isNaN(secondNum)) {
   firstNum = Number(firstNum);
   secondNum = Number(secondNum);
-  
-  if (secondNum === 10 || firstNum < secondNum) {
-    console.log(firstNum);
-  }
-  
-  else {
-    let result = '';
-    while (firstNum >= secondNum) {
-      result =
-        (firstNum - Math.floor(firstNum / secondNum) * secondNum).toString() +
-        result;
-      firstNum = Math.floor(firstNum / secondNum);
 
-      if (firstNum < secondNum) {
-        result = firstNum.toString() + result;
-      }
-    
-    }
-    console.log(Number(result));
-  }
-}
-
-else {
+  console.log(firstNum.toString(secondNum))
+} else {
   console.log("Некорректный ввод!");
 }
