c5c8bd353e763b82238e45028df4fd22e2ed8c3e
Nik <133938672+NIK20-58@users.noreply.github.com>
2024-01-01 23:03:32 +0400

    Update ДЗ-2 задание№2 Iurtaev Nikita.js

--- ДЗ-2 задание№2 Iurtaev Nikita.js
+++ ДЗ-2 задание№2 Iurtaev Nikita.js
@@ -1,5 +1,4 @@
 function selectFromInterval(arr,startNum,endNum) {
-  
   if (Array.isArray(arr) && typeof startNum === 'number' && typeof endNum === 'number' && !isNaN(startNum) && !isNaN(endNum) && isFinite(startNum) && isFinite(endNum)) {
 
     if (startNum > endNum) {
@@ -9,24 +8,20 @@ function selectFromInterval(arr,startNum,endNum) {
     }
 
     const result = [];
+    
     for (let i=0;i<arr.length;i++) {
+      
       if (typeof arr[i] === 'number' && !isNaN(arr[i]) && isFinite(arr[i])) {
-
         if (startNum <= arr[i] && endNum >= arr[i]) {
           result.push(arr[i]);
         }
-
-      }
-      
-      else {
+      } else {
         throw new Error('Ошибка!');
       }
     }
+    
     return result;
-  }
-  
-  else {
+  } else {
     throw new Error('Ошибка!');
   }
-
 }
