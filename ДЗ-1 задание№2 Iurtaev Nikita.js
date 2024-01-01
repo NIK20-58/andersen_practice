6af38641ef189f65420e850047592de601aaec20
Nik <133938672+NIK20-58@users.noreply.github.com>
2024-01-01 22:42:24 +0400

    Update ДЗ-1 задание№2 Iurtaev Nikita.js

--- ДЗ-1 задание№2 Iurtaev Nikita.js
+++ ДЗ-1 задание№2 Iurtaev Nikita.js
@@ -1,23 +1,14 @@
 const ERROR_MESSAGE = "Некорректный ввод!";
 let firstNum = prompt();
 
-
 if (firstNum.trim() !== '' && !isNaN(firstNum)) {
   let secondNum = prompt();
   
-  
   if (secondNum.trim() !== '' && !isNaN(secondNum) && secondNum !== '0') {
-    console.log(
-      `Ответ: ${Number(firstNum) + Number(secondNum)}, ${Number(firstNum) / Number(secondNum)}.`
-    );
-    
-  } 
-  
-  else {
+    console.log(`Ответ: ${Number(firstNum) + Number(secondNum)}, ${Number(firstNum) / Number(secondNum)}.`);  
+  } else {
     console.log(ERROR_MESSAGE);
   }
-} 
-
-else {
+} else {
   console.log(ERROR_MESSAGE);
 }
